cfac6e0d8f7594f234d4d5580ba76348bada774c
Rohit Nair <6954311+rnair98@users.noreply.github.com>
2022-12-05 11:52:58 -0500

    updates to css for sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,15 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
+import './Sidebar.css'
 
 function Sidebar() {
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
         <div className="sidebar__top">
-            <img src="" alt="" />
-            <Avatar className="sidebar_-avatar" />
+            <img 
+                src="https://images.unsplash.com/photo-1669920687377-537d98f0d837?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80" 
+                alt="background" />
+            <Avatar className="sidebar__avatar" />
             <h2>Victor Sullivan</h2>
             <h4>sully@xyz.com</h4>
         </div>
